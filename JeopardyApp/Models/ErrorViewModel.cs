e9ffa76808d910bb124fbae1900d643715be647f
EulloJohnRaven <cancergaming123456@gmail.com>
2026-09-10 10:43:16 +0800

    Commit 1 Setup Jeopardyapp

---
+++ JeopardyApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JeopardyApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
