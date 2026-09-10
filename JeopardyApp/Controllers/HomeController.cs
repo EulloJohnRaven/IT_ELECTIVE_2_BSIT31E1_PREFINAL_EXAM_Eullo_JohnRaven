d6eaa995019c6ff609b11639848c78d39ca12597
EulloJohnRaven <cancergaming123456@gmail.com>
2026-09-10 11:08:33 +0800

    Question 4

--- JeopardyApp/Controllers/HomeController.cs
+++ JeopardyApp/Controllers/HomeController.cs
@@ -13,6 +13,7 @@ namespace JeopardyApp.Controllers
                 new Flashcard { Id = 1, Question = "What is the main problem solved by using a database instead of an in-memory collection?", Answer = "C. It allows data to persist after the application stops"},
                 new Flashcard { Id = 2, Question = "Which approach is being used when an existing database is used to generate EF Core entity classes?", Answer = "B. Database-First" },
                 new Flashcard { Id = 3, Question = "What is the primary purpose of Entity Framework Core?", Answer = "C. To map objects in code to relational database data" },
+                new Flashcard { Id = 4, Question = "Which EF Core component is primarily responsible for communicating with the database?", Answer = "A. DbContext"},
 
             };
 
