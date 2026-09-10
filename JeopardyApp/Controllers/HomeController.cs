f74957ca1dd072c0ddc5f8d2c9bda7ea18d08456
EulloJohnRaven <cancergaming123456@gmail.com>
2026-09-10 11:16:35 +0800

    Question 14

--- JeopardyApp/Controllers/HomeController.cs
+++ JeopardyApp/Controllers/HomeController.cs
@@ -23,6 +23,7 @@ namespace JeopardyApp.Controllers
                 new Flashcard { Id = 11, Question = "Why might a ViewModel be used when displaying Student and Section information?",Answer = "B. To combine or shape the data specifically needed by the view"},
                 new Flashcard { Id = 12, Question = "What is the main benefit of Include(s => s.Section)?", Answer = "A. It loads the related Section navigation property" },
                 new Flashcard { Id = 13, Question = "Which type of validation occurs in the browser before a request is sent to the server?", Answer = "B. Client-side validation" },
+                new Flashcard { Id = 14, Question = "Why is server-side validation still necessary if client-side validation exists?", Answer = "A. Client-side validation can be bypassed"},
 
             };
 
