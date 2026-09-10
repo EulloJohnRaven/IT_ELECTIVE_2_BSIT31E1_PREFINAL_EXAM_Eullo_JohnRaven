ff5c3e223002ec84d17bca447088bbe2c97c7b8c
EulloJohnRaven <cancergaming123456@gmail.com>
2026-09-10 11:10:13 +0800

    Question 6

--- JeopardyApp/Controllers/HomeController.cs
+++ JeopardyApp/Controllers/HomeController.cs
@@ -15,6 +15,7 @@ namespace JeopardyApp.Controllers
                 new Flashcard { Id = 3, Question = "What is the primary purpose of Entity Framework Core?", Answer = "C. To map objects in code to relational database data" },
                 new Flashcard { Id = 4, Question = "Which EF Core component is primarily responsible for communicating with the database?", Answer = "A. DbContext"},
                 new Flashcard { Id = 5, Question = "What does the following command primarily do? dotnet ef dbcontext scaffold...", Answer = "C. Generates EF Core models and a DbContext from an existing database"},
+                new Flashcard { Id = 6, Question = "Where is a database connection string commonly stored in an ASP.NET Core MVC application?", Answer = "B. appsettings.json" },
 
             };
 
