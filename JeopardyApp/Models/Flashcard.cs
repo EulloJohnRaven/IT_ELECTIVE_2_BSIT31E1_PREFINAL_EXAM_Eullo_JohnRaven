53c47580867488e58ade6f31b8c0ba3c82b280ac
EulloJohnRaven <cancergaming123456@gmail.com>
2026-09-10 10:47:14 +0800

    Flashcard Model Created

---
+++ JeopardyApp/Models/Flashcard.cs
@@ -0,0 +1,9 @@
+﻿namespace JeopardyApp.Models
+{
+    public class Flashcard
+    {
+        public int Id { get; set; }
+        public string? Question { get; set; }
+        public string? Answer { get; set; }
+    }
+}
